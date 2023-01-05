e1a35d6deeae93654a7ac42b0e6efa14da6167bf
John Peter Bielawa <janbielawa00@gmail.com>
2023-01-05 13:06:30 +0100

    const bool& stopEarly Added new attributes to the class
    Major additons:
    - Game: Player class now can store information about: ammount of bets
      player have mad and money they have cummulated.
      - Game: Setters and Getters were included
    - Display: Player class now can display ammount of money they lost or
      won

--- application/code/game/include/Player.h
+++ application/code/game/include/Player.h
@@ -5,17 +5,26 @@
 class Player
 {
     public:
-    
+    void displayMoneyWonLoss();
+
+    // Setters
     void setNickName(const std::string& nickName) { nickName_ = nickName;}
     void setBalance(const int& balance) { balance_ = balance;}
+    void setMoneyAccumulated(const int& moneyAccumulated) {moneyAccumulated_ = moneyAccumulated;}
+    void setBetAmmount(const uint32_t& betAmmount) {betAmmount_ = betAmmount;}
+
+    // Getters
     std::string getNickName() const {return nickName_;}
     int getBalance() const {return balance_;}
+    int getMoneyAccumulated() const {return moneyAccumulated_;}
 
     Player() = default;
-    Player(int balance) {balance_ = balance;}
-    Player(std::string nickName, int balance) {nickName_ = nickName; balance_ = balance;}
+    Player(int balance) {balance_ = balance; moneyAccumulated_ = 0;}
+    Player(std::string nickName, int balance) {nickName_ = nickName; balance_ = balance; moneyAccumulated_ = 0;}
     private:
+    uint32_t betAmmount_;
     std::string nickName_;
     int balance_;
+    int moneyAccumulated_;
 
 };
--- application/code/game/src/Player.cpp
+++ application/code/game/src/Player.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
+
 #include "../include/Player.h"
 
+void Player::displayMoneyWonLoss()
+{
+    std::cout << "Player " << nickName_;
+    if(moneyAccumulated_ < 0)
+    {
+        std::cout << " have lost: " << moneyAccumulated_ 
+        << std::endl;
+    }
+    else if(moneyAccumulated_ > 0)
+    {
+        std::cout << " have won: " << moneyAccumulated_ 
+        << std::endl;
+    }
+}
