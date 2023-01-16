8eda26ddc741227236319e28f17b93824532cf59
John Peter Bielawa <janbielawa00@gmail.com>
2023-01-16 14:03:42 +0100

    `Player` now stores global values
    Major Addition:
    - In order to maintain information about player real stats, global
      variables were implemented. Their goal is to cumulate every atribute
      and store it correctly in file.
    
    Major Change:
    - Every constructor has been change to start with correct global values.
    
    Activity: Improvement

--- application/code/game/include/Player.h
+++ application/code/game/include/Player.h
@@ -7,33 +7,55 @@ class Player
     public:
     void displayMoneyWonLoss();
     void displayBetPassCounts();
+    void moveToGlobalStats();
 
     // Setters
-    void setNickName(const std::string& nickName) { nickName_ = nickName;}
+    // Locals
     void setBalance(const int& balance) { balance_ = balance;}
     void setMoneyAccumulated(const int& moneyAccumulated) {moneyAccumulated_ = moneyAccumulated;}
-    void setBetAmmount(const uint32_t& betAmmount) {betAmmount_ = betAmmount;}
+    void setGoodBetCount(const uint32_t& goodBetCount) {goodBetCount_ = goodBetCount;}
     void setBetCount(const uint32_t& betCount) {betCount_ = betCount;}
     void setPassCount(const uint32_t& passCount) {passCount_ = passCount;}
+    // Globals
+    void setNickName(const std::string& nickName) { nickName_ = nickName;}
+    void setGlobalMoneyAccumulated(const int& globMoneyAccumulated) {globMoneyAccumulated_ = globMoneyAccumulated;}
+    void setGlobalGoodBetCount(const uint32_t& globGoodBetCount) {globGoodBetCount_ = globGoodBetCount;}
+    void setGlobalBetCount(const uint32_t& globBetCount) {globBetCount_ = globBetCount;}
+    void setGlobalPassCount(const uint32_t& globPassCount) {globPassCount_ = globPassCount;}
 
     // Getters
-    std::string getNickName() const {return nickName_;}
+    // Locals
     int getBalance() const {return balance_;}
     int getMoneyAccumulated() const {return moneyAccumulated_;}
+    uint32_t getGoodBetCount() const {return goodBetCount_;}
     uint32_t getBetCount() const {return betCount_;}
     uint32_t getPassCount() const {return passCount_;}
+    // Globals
+    std::string getNickName() const {return nickName_;}
+    int getGlobalMoneyAccumulated() const {return globMoneyAccumulated_;}
+    uint32_t getGlobalGoodBetCount() const {return globGoodBetCount_;}
+    uint32_t getGlobalBetCount() const {return globBetCount_;}
+    uint32_t getGlobalPassCount() const {return globPassCount_;}
 
     Player() = default;
-    /// @brief Constructor used as default when initial value of balance is set
-    /// @param balance inistial value of bank ammount 
-    Player(int balance) {balance_ = balance; moneyAccumulated_ = 0; betCount_ = 0; passCount_ = 0;}
-    Player(std::string nickName, int balance) {nickName_ = nickName; balance_ = balance; moneyAccumulated_ = 0;
-                                               betCount_ = 0; passCount_ = 0;}
+    Player(int balance);
+    Player(const std::string nickName, int balance);                                    
+    Player(const std::string nickName, int globMoneyAccumulated, uint32_t goodBetCount, uint32_t betCount,
+           uint32_t passCount, int balance = 0);
+
     private:
-    uint32_t betAmmount_;
-    std::string nickName_;
+    // Local variables related to one session
     int balance_;
     int moneyAccumulated_;
+    uint32_t goodBetCount_;
     uint32_t betCount_;
     uint32_t passCount_;
+
+    // Global variables
+    std::string nickName_;
+    int globMoneyAccumulated_;
+    uint32_t globGoodBetCount_;
+    uint32_t globBetCount_;
+    uint32_t globPassCount_;
+
 };
--- application/code/game/src/Player.cpp
+++ application/code/game/src/Player.cpp
@@ -38,3 +38,73 @@ void Player::displayBetPassCounts()
         std::cout << "no passes!\n";
     }
 }
+
+/// @brief Constructor used for saving and loading
+/// @param nickName user name
+/// @param globMoneyAccumulated how many player have accumulated during all of his playthroughs
+/// @param globGoodBetCount how many bets that player placed were good
+/// @param globBetCount how many bets in general player have placed
+/// @param globPassCount how many passes he has done
+/// @param balance initial bank balance (at start of the game) / default is 0
+/// @note balance is optional flag
+Player::Player(const std::string nickName, int globMoneyAccumulated, uint32_t globGoodBetCount, uint32_t globBetCount,
+               uint32_t globPassCount, int balance)
+{
+    // Setup global variables
+    nickName_ = nickName;
+    globBetCount_ = globBetCount;
+    globGoodBetCount_ = globGoodBetCount;
+    globPassCount_ = globPassCount;
+
+    // Setup local variables
+    balance_ = balance;
+    moneyAccumulated_ = 0;
+    passCount_ = 0;
+    betCount_ = 0;
+    goodBetCount_ = 0;
+
+}
+
+/// @brief Constructor used as default when initial value of balance is set
+/// @param balance inistial value of bank ammount 
+Player::Player(int balance)
+{
+    // Global
+    nickName_ = "";
+    globBetCount_ = 0;
+    globGoodBetCount_ = 0;
+    globMoneyAccumulated_ = 0;
+    globPassCount_ = 0;
+
+    // Local
+    balance_ = balance;
+    moneyAccumulated_ = 0;
+    betCount_ = 0;
+    passCount_ = 0;
+    goodBetCount_ = 0;
+}
+
+Player::Player(const std::string nickName, int balance)
+{
+    // Global
+    nickName_ = nickName; 
+    globBetCount_ = 0;
+    globGoodBetCount_ = 0;
+    globMoneyAccumulated_ = 0;
+    globPassCount_ = 0;
+    
+    // Local
+    balance_ = balance;
+    moneyAccumulated_ = 0;
+    betCount_ = 0;
+    passCount_ = 0;
+    goodBetCount_ = 0;
+}
+
+void Player::moveToGlobalStats()
+{
+    globPassCount_ += passCount_;
+    globBetCount_ += betCount_;
+    globGoodBetCount_ += goodBetCount_;
+    globMoneyAccumulated_ += moneyAccumulated_;
+}
