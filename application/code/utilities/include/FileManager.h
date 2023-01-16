f6f185d2004c3a56847390864f19f9b48b95f647
John Peter Bielawa <janbielawa00@gmail.com>
2023-01-16 14:10:55 +0100

    File manager now uses touch to create files
    Major Addition:
    - Utilities: FileManager now uses touch to create previously presseted
      text file.
    - Utilities: FileType enum has been added in order to keep track on
      which touch preset template use to generate a file.
    
    Activity: Improvement

--- application/code/utilities/include/FileManager.h
+++ application/code/utilities/include/FileManager.h
@@ -1,22 +1,36 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <map>
 
 #include "./Paths.h"
+#include "../../game/include/Player.h"
 
+/*
 /// @brief This map stores all the information about every file necessery to launch the game.
 /// @note Every file added or deleted must be stored here along with its path
 /// @warning Init.cfg is not included in this list
 /// @details Map stores information about the following: expected file path (key value); file name decleared as pair (second value)
 std::map<const std::string, const std::string> _filesToCheck{
 
+};*/
+
+enum FileType{
+    PlayerStat = 1,
+    GameSave = 2,
+    GameSaveDbg = 3
 };
 
 class FileManager{
     public:
     bool isFileGood(const std::string& path, const std::string& fileName);
-    void createFile(const std::string& path, const std::string& filename);
+    void createFile(const std::string& path, const std::string& fileName);
+
+    // Touch function is used to create already presseted file 
+    void touch(const FileType& fTypem, const std::string& fileName);
+    void appendPlayerSaveFile(const Player& appPlayerStat);
+    std::vector <std::string> loadFileContent(const std::string& path, const std::string& fileName);
 
     FileManager() = default;
 };
--- application/code/utilities/src/FileManager.cpp
+++ application/code/utilities/src/FileManager.cpp
@@ -15,9 +15,9 @@ bool FileManager::isFileGood(const std::string& path, const std::string& fileNam
 }
 
 
-void FileManager::createFile(const std::string& path, const std::string& filename)
+void FileManager::createFile(const std::string& path, const std::string& fileName)
 {
-    const std::string toMake = path + "/" + filename;
+    const std::string toMake = path + "/" + fileName;
     std::ofstream makeFile;
     makeFile.open(toMake);
     if(makeFile.is_open())
@@ -27,4 +27,93 @@ void FileManager::createFile(const std::string& path, const std::string& filenam
     }
     
     std::cout << "Could not create the file!\n";
+}
+
+void FileManager::touch(const FileType& fType, const std::string& fileName)
+{
+    std::string tmpPathHolder;
+    std::string filePath;
+    std::ofstream makeFile;    
+    switch (fType)
+    {
+    case PlayerStat:
+        createFile(FILE_PLAYER_STATS_PATH, fileName);
+        tmpPathHolder = FILE_PLAYER_STATS_PATH;
+        filePath = tmpPathHolder + "/" + fileName;
+        makeFile.open(filePath);
+        makeFile << "Name:\nGoodBetCount:\nPassCount:\nBetCount:\nTotalMoneyGained:\n";
+        makeFile.close();
+        break;
+    case GameSave:
+        createFile(FILE_GAME_SAVE_LOG, fileName);
+        break;
+    case GameSaveDbg:
+        createFile(FILE_GAME_SAVES_LOG_DBG, fileName);
+        break;
+    default:
+        std::cout << "No such file type in preset templates. Use createFile!\n";
+        break;
+    }
+}
+
+void FileManager::appendPlayerSaveFile(const Player& appPlayerStat)
+{
+    std::string path = FILE_PLAYER_STATS_PATH;
+    std::vector<std::string> appendContent = loadFileContent(FILE_PLAYER_STATS_PATH, 
+                                                             appPlayerStat.getNickName() + EXT_PLAYER_STATS);
+    std::ofstream appendTheSave;
+    appendTheSave.open(path + "/" + appPlayerStat.getNickName() + EXT_PLAYER_STATS);
+    uint16_t lineNum = 0;
+
+    for(auto& line : appendContent)
+    {
+        // Delete old values
+        line.erase(line.begin() + line.rfind(':') + 1, line.end());
+        // Add new values
+        appendTheSave << line;
+        switch (lineNum)
+        {
+        case 0:
+            appendTheSave << appPlayerStat.getNickName();
+            break;
+        case 1:
+            appendTheSave << appPlayerStat.getGlobalGoodBetCount();
+            break;            
+        case 2:
+            appendTheSave << appPlayerStat.getGlobalBetCount();
+            break;            
+        case 3:
+            appendTheSave << appPlayerStat.getGlobalPassCount();
+            break;            
+        case 4:
+            appendTheSave << appPlayerStat.getGlobalMoneyAccumulated();
+            break;
+        
+        default:
+        // Eof
+            break;
+        }
+        appendTheSave << std::endl;
+        lineNum++;
+    }
+    appendTheSave.close();
+}
+
+std::vector <std::string> FileManager::loadFileContent(const std::string& path, const std::string& fileName)
+{
+    std::vector<std::string> fileContent;
+    std::ifstream readFile;
+    std::string lineInfo;
+    // No need to include EXT_PLAYER_STATS as it is already being passed with this
+    readFile.open(path + "/" + fileName);
+    if(!readFile.is_open())
+    {
+        std::cout << "Something went wrong!\n";
+    }
+    while(std::getline(readFile, lineInfo))
+    {
+        fileContent.push_back(lineInfo);
+    }
+    readFile.close();
+    return fileContent;
 }
