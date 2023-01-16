394f6474ce4677b0ca8d8a7affb6d3a544ffb02e
John Peter Bielawa <janbielawa00@gmail.com>
2023-01-16 14:07:41 +0100

    Added file paths and default file extensions
    Major addition:
    - Utilities: Every file now requires it's extension to be implemented in this file.
    
    Minor additions:
    - Utilities: Every folder with for file has been added
    - Utilities: New file macro FILE_PLAYER_HIGHSCORES

--- application/code/utilities/include/Paths.h
+++ application/code/utilities/include/Paths.h
@@ -2,11 +2,20 @@
 // Folders and subfolders
 #define FOLDER_BIN "bin"
 #define FOLDER_BIN_SAVES "bin\\saves"
+#define FOLDER_BIN_SAVES_DEBUG "bin\\saves\\debug"
 #define FOLDER_BIN_PLAYERS_STATS "bin\\player\\stats"
-#define FOLDER_BIN_UTILIS "bin\\utilities"
 
 // Files paths
-#define INIT_CONFIG_PATH "./bin"
+#define INIT_CONFIG_PATH "bin"
+#define FILE_PLAYER_HIGHSCORES_PATH "bin/player"
+#define FILE_PLAYER_STATS_PATH "bin/player/stats"
+#define FILE_GAME_SAVE_LOG "bin/saves"
+#define FILE_GAME_SAVES_LOG_DBG "bin/saves/debug"
 
 // File names
 #define INIT_CONFIG_FILE "init.cfg"
+#define FILE_PLAYER_HIGHSCORES "highscores.csv"
+
+
+// File extensions
+#define EXT_PLAYER_STATS ".pl"
